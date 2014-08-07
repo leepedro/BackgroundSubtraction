80e46794fd513cc4edde9de0d2d85e178ba46ef6
Cheolha Pedro Lee <leepedro@gmail.com>
2014-08-07 11:33:30 -0400

    added a sorting feature

--- BackgroundSuppression_1/background_suppression_1.cpp
+++ BackgroundSuppression_1/background_suppression_1.cpp
@@ -3,15 +3,18 @@
 #include <wincodec.h>
 #include <ShlObj.h>
 
+// Standard C++ header files.
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 void LoadFileList(std::wstring &pathFolder, std::vector<std::wstring> &filenames)
 {
 	::IFileDialog *file_dialog(nullptr);
 	if (SUCCEEDED(::CoCreateInstance(__uuidof(::FileOpenDialog), nullptr, CLSCTX_ALL, IID_PPV_ARGS(&file_dialog))))
 	{
+		// Set the options for a file dialog to show only folders.
 		::FILEOPENDIALOGOPTIONS options;
 		if (SUCCEEDED(file_dialog->GetOptions(&options)))
 		{
@@ -21,14 +24,19 @@ void LoadFileList(std::wstring &pathFolder, std::vector<std::wstring> &filenames
 				HRESULT result = file_dialog->Show(nullptr);
 				if (SUCCEEDED(result))
 				{
+					// Get the path of the selected folder.
 					::IShellItem *selected_item(nullptr);
 					if (SUCCEEDED(file_dialog->GetResult(&selected_item)))
 					{
 						wchar_t *path_folder(nullptr);
+						// NOTE: if multi-threading option is selected for COM initialization,
+						// the following function fails for the user library. Don't know why.
 						if (SUCCEEDED(selected_item->GetDisplayName(::SIGDN_FILESYSPATH, &path_folder)))
 						{
 							pathFolder = path_folder;
 							::MessageBoxW(nullptr, path_folder, L"Selected Folder", MB_OK);//
+
+							// Collected the list of files under the selected folder.
 							::WIN32_FIND_DATAW find_data;
 							std::wstring search_pattern = std::wstring(path_folder) + L"\\*";
 							HANDLE hFindFile = ::FindFirstFileW(search_pattern.c_str(), &find_data);
@@ -36,24 +44,27 @@ void LoadFileList(std::wstring &pathFolder, std::vector<std::wstring> &filenames
 							{
 								do
 								{
+									// Skip {., .., sub-folders} for now, and collect only filenames at the selected folder.
 									if (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-									{
 										std::wclog << L"Skipping " << find_data.cFileName << std::endl;//
-										// Skip {., .., sub-directories} for now.
-									}
 									else
 										filenames.push_back(std::wstring(find_data.cFileName));
 								}									
 								while (::FindNextFileW(hFindFile, &find_data));
+
 								::FindClose(hFindFile);
+
+								// Sort the list of filenames.
+								std::sort(filenames.begin(), filenames.end());
 							}
 							else
-								::MessageBoxW(nullptr, L"Failed to get the result from a file dialog.", L"Error", MB_OK);
+								::MessageBoxW(nullptr, L"Failed to find the first file in the selected folder.", L"Error", MB_OK);
 							
 							::CoTaskMemFree(path_folder);
 						}
 						else
 							::MessageBoxW(nullptr, L"Failed to get the path of the selected folder.", L"Error", MB_OK);
+
 						selected_item->Release();
 					}
 					else
@@ -68,6 +79,8 @@ void LoadFileList(std::wstring &pathFolder, std::vector<std::wstring> &filenames
 		}
 		else
 			::MessageBoxW(nullptr, L"Failed to get the options of a file dialog.", L"Error", MB_OK);
+
+		file_dialog->Release();
 	}
 	else
 		::MessageBoxW(nullptr, L"Failed to create a file dialog.", L"Error", MB_OK);
@@ -75,13 +88,14 @@ void LoadFileList(std::wstring &pathFolder, std::vector<std::wstring> &filenames
 
 int main(void)
 {
-	// NOTE: if multi-threading option is selected for COM initialization, it can not recognize files in user library. Don't know why.
+	// NOTE: if multi-threading option is selected for COM initialization, it can not recognize files in the user library. Don't know why.
 	if (SUCCEEDED(::CoInitializeEx(nullptr, ::COINIT_APARTMENTTHREADED | ::COINIT_DISABLE_OLE1DDE)))
 	//if (SUCCEEDED(::CoInitializeEx(nullptr, ::COINIT_MULTITHREADED | ::COINIT_DISABLE_OLE1DDE)))
 	{
 		std::vector<std::wstring> filenames;
 		std::wstring path_folder;
 		LoadFileList(path_folder, filenames);
+
 		::CoUninitialize();
 	}
 }
