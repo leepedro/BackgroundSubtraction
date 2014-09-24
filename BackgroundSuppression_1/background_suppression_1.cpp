e0a5bcac33766047fc7fb8fcd08cb55557b68995
Cheolha Pedro Lee <leecheolha@gmail.com>
2014-09-24 02:18:00 -0400

    Added BGRAtoGray(). Added timer feature.

--- BackgroundSuppression_1/background_suppression_1.cpp
+++ BackgroundSuppression_1/background_suppression_1.cpp
@@ -3,6 +3,9 @@
 #include <wincodec.h>
 #include <ShlObj.h>
 
+// Standard C header files.
+#include <ctime>
+
 // Standard C++ header files.
 #include <string>
 #include <vector>
@@ -71,7 +74,7 @@ void LoadFileList(std::wstring &pathFolder, std::vector<std::wstring> &filenames
 					else
 						::MessageBoxW(nullptr, L"Failed to get the result from a file dialog.", L"Error", MB_OK);
 				}
-				else if (result == ::HRESULT_FROM_WIN32(ERROR_CANCELLED)) {}	// Do nothing if cancelled.
+				else if (result == ::HRESULT_FROM_WIN32(ERROR_CANCELLED)) {}	// Do nothing if canceled.
 				else
 					::MessageBoxW(nullptr, L"Failed to get a response from a file dialog.", L"Error", MB_OK);
 			}
@@ -143,6 +146,30 @@ void LoadImageFile(const std::wstring &pathSrc, std::vector<unsigned char> &data
 
 }
 
+void BGRAtoGray(const std::vector<unsigned char> &src, std::vector<float> &dst)
+{
+	if (dst.size() != (src.size() / 4))
+		dst.resize(src.size() / 4, 0.0f);
+
+	auto it_src = src.cbegin();
+	for (auto it_dst = dst.begin(); it_dst != dst.end(); ++it_dst)
+	{
+		*it_dst = *it_src++;	// B
+		*it_dst += *it_src++;	// G
+		*it_dst += *it_src++;	// R
+		*it_dst /= 3.0f;
+		++it_src;				// Skip A
+	}
+	//std::for_each(dst.begin(), dst.end(), [&it_src](double &value)
+	//{
+	//	value = *it_src++;		// B
+	//	value += *it_src++;		// G
+	//	value += *it_src++;		// R
+	//	value /= 3.0;
+	//	++it_src;				// Skip A
+	//});	
+}
+
 void ComputeMean(const std::deque<std::vector<unsigned char>> &buffer, std::vector<double> &result)
 {
 	// Initialize the output data based on the size of the first vector in the input buffer.
@@ -159,12 +186,37 @@ void ComputeMean(const std::deque<std::vector<unsigned char>> &buffer, std::vect
 	std::for_each(result.begin(), result.end(), [NUM_FRMS](double &value) { value /= NUM_FRMS; });
 }
 
+void ComputeMean(const std::deque<std::vector<float>> &buffer, std::vector<float> &result)
+{
+	// Initialize the output data based on the size of the first vector in the input buffer.
+	::size_t sz = buffer.cbegin()->size();
+	if (result.size() != sz)
+		result.resize(sz, 0.0f);
+
+	// Accumulate all vectors in the input buffer to the output data.
+	for (const auto &data : buffer)
+		std::transform(data.cbegin(), data.cend(), result.begin(), result.begin(), std::plus<float>());
+
+	// Divide the output data by the length of the input buffer. 
+	const float NUM_FRMS = static_cast<float>(buffer.size());
+	std::for_each(result.begin(), result.end(), [NUM_FRMS](float &value) { value /= NUM_FRMS; });
+}
+
+
 void ComputeDiff(const std::vector<unsigned char> &a, const std::vector<double> &b, std::vector<double> &result)
 {
 	if (result.size() != b.size())
 		result.resize(b.size());
 	//std::transform(a.cbegin(), a.cend(), b.cbegin(), result.begin(), std::minus<double>());
 	std::transform(a.cbegin(), a.cend(), b.cbegin(), result.begin(), [](unsigned char a_, double b_) { return std::abs(a_ - b_); });
+}
+
+void ComputeDiff(const std::vector<float> &a, const std::vector<float> &b, std::vector<float> &result)
+{
+	if (result.size() != a.size())
+		result.resize(a.size());
+	//std::transform(a.cbegin(), a.cend(), b.cbegin(), result.begin(), std::minus<double>());
+	std::transform(a.cbegin(), a.cend(), b.cbegin(), result.begin(), [](float a_, float b_) { return std::abs(a_ - b_); });
 }
 
 int main(void)
@@ -176,20 +228,25 @@ int main(void)
 		::IWICImagingFactory *wic_factory(nullptr);	// __uuidof(IWICImagingFactory)
 		if (SUCCEEDED(::CoCreateInstance(::CLSID_WICImagingFactory, nullptr, CLSCTX_ALL, IID_PPV_ARGS(&wic_factory))))
 		{
+			// Load which folder to read files.
 			std::vector<std::wstring> filenames;
 			std::wstring path_folder;
 			LoadFileList(path_folder, filenames);
-			const ::size_t MAX_BUFFER_LENGTH(5);
-			std::deque<std::vector<unsigned char>> buffer;		
 
+			auto t_start = ::clock();
+			const ::size_t MAX_BUFFER_LENGTH(5);
+			std::deque<std::vector<float>> buffer;		
+			std::vector<unsigned char> src_data;
+			std::vector<float> avg, diff;
 			for (const auto &filename: filenames)
 			{
 				// Load an image file.
 				std::wstring path_src = path_folder + L"\\" + filename;
-				std::vector<unsigned char> data;
-				LoadImageFile(path_src, data, wic_factory);
+				//std::vector<unsigned char> src_data;
+				LoadImageFile(path_src, src_data, wic_factory);
 
-				// TODO: Push single-channel images to the frame buffer either by averaging or subtracting frames.
+				std::vector<float> data;
+				BGRAtoGray(src_data, data);
 
 				// Push the data to a buffer.
 				if (buffer.size() == MAX_BUFFER_LENGTH)
@@ -197,10 +254,14 @@ int main(void)
 				buffer.push_back(std::move(data));
 
 				// Do something.
-				std::vector<double> avg, diff;
+				//std::vector<float> avg, diff;
 				ComputeMean(buffer, avg);
 				ComputeDiff(buffer.back(), avg, diff);
 			}
+
+			auto t_end = ::clock();
+			auto sec_total = static_cast<double>(t_end - t_start) / CLOCKS_PER_SEC;
+			std::clog << "Total computation time = " << sec_total << " (sec)" << std::endl;
 
 			wic_factory->Release();
 		}
